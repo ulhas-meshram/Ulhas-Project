2029ddf6417261b36025bef3abc6a07172de2b96
ulhas-meshram <117505943+ulhas-meshram@users.noreply.github.com>
2023-05-17 15:45:37 +0530

    Add files via upload

---
+++ pythonsdks3 create S3 bucket.py
@@ -0,0 +1,15 @@
+# import boto3
+# s3 = boto3.client("s3",aws_access_key_id = "AKIAW4CFPWVH6H7P4T6H",
+#                   aws_secret_access_key = "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",
+#                   region_name = "us-east-1")
+#
+# responce = s3.create_bucket(Bucket="ulhas-sdk-boto3")
+# print(responce)
+
+#
+# import boto3
+# s3 = boto3.client("s3",aws_access_key_id = "AKIAW4CFPWVH6H7P4T6H",
+#                   aws_secret_access_key = "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",
+#                   region_name = "us-east-1")
+# r = s3.list_objects(Bucket = 'ulhas-sdk-boto3' )
+# print(r)
